c56de22d5b0ea6c9e9a7598ae45316de1b6f2323
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2025-06-05 13:15:27 +0200

    Passwords for users

--- src/backend/modules/user/user.types.ts
+++ src/backend/modules/user/user.types.ts
@@ -1,12 +1,12 @@
 export type User = {
   id: string;
   email: string;
+  password: string;
   name: string | null;
   role: "ADMIN" | "LOW_PRIVILEGED_USER";
   createdAt: Date;
 };
 
-export type SanitizedUserResponse = User;
-// export type SanitizedUserResponse = Omit<User, "password">;
+export type SanitizedUserResponse = Omit<User, "password">;
 export type CreateUserInput = Omit<User, "id" | "createdAt">;
 export type UpdateUserInput = Partial<CreateUserInput> & { id: string };
