f2728b7ff2d2b142c0f45c24882fddc4e66f7718
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2025-06-08 20:47:48 +0200

    Get Users as Admin

--- src/app/(backend)/api/private/users/route.ts
+++ src/app/(backend)/api/private/users/route.ts
@@ -1,7 +1,10 @@
+import { requireRole } from "@/backend/auth/auth.guard";
 import { UserController } from "@/backend/modules/user/user.controller";
 import { handleGetRoute } from "@/backend/routeHandlers/get.route";
+import { UserRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export const GET = handleGetRoute(async () => {
+  await requireRole(UserRole.ADMIN);
   return NextResponse.json(await UserController.getUsers(), { status: 200 });
 });
--- src/app/(frontend)/(admin)/admin-panel/users/page.tsx
+++ src/app/(frontend)/(admin)/admin-panel/users/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { useEffect } from "react";
+
 export default function UsersPage() {
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("/api/private/users");
+      const json = await res.json();
+
+      console.log(res);
+      console.log(json);
+    } catch {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return <h1>Użytkownicy</h1>;
 }
